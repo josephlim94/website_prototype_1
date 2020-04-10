67aeb4ef01bf3afac30662d4e807f1d8b04ee7b6
josephlim94 <josephlim_94@hotmail.co.uk>
2020-04-10 13:13:20 +0800

    rest api serializer tutorial

--- website_prototype_1/urls.py
+++ website_prototype_1/urls.py
@@ -34,4 +34,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^', include(router.urls)),
     url(r'api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('snippets/', include('snippets.urls')),
 ]
