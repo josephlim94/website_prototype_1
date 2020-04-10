94005a8a033258a3850d0906aeecdb79e0fcc09c
josephlim94 <josephlim_94@hotmail.co.uk>
2020-04-10 13:13:47 +0800

    database design

--- main_app/models.py
+++ main_app/models.py
@@ -17,3 +17,23 @@ class ListingData(models.Model):
     proposed_date = models.DateTimeField('proposed date')
     def __str__(self):
         return self.item_of_interest
+
+class GeneralTable(models.Model):
+    listing_foreign_key = models.ForeignKey(ListingData, on_delete=models.CASCADE)
+    general_data = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+
+class DateTable(models.Model):
+    listing_foreign_key = models.ForeignKey(ListingData, on_delete=models.CASCADE)
+    date_data = models.DateField()
+    description = models.CharField(max_length=200)
+
+class TimeTable(models.Model):
+    listing_foreign_key = models.ForeignKey(ListingData, on_delete=models.CASCADE)
+    time_data = models.TimeField()
+    description = models.CharField(max_length=200)
+
+class PriceTable(models.Model):
+    listing_foreign_key = models.ForeignKey(ListingData, on_delete=models.CASCADE)
+    price_data = models.FloatField(default=0)
+    description = models.CharField(max_length=200)
