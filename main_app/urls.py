0f1a68afa8126591b30375556e76c97ab98d3568
josephlim94 <josephlim_94@hotmail.co.uk>
2020-04-13 11:45:07 +0800

    remove example views

--- main_app/urls.py
+++ main_app/urls.py
@@ -9,14 +9,7 @@ from organizations.views import OrganizationCreate
 
 app_name = 'main_app'
 urlpatterns = [
-    # ex: /main_app/
     path('', views.index, name='index'),
-    # ex: /main_app/5/
-    path('<int:question_id>/', views.detail, name='detail'),
-    # ex: /main_app/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
-    # ex: /main_app/5/vote/
-    path('<int:question_id>/vote/', views.vote, name='vote'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('user_login/', views.user_login, name='user_login'),
     path('user_logout/', views.user_logout, name='user_logout'),
